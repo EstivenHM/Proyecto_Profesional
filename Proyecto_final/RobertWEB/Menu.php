74d7d7c9bd01d41a1fa1db019de202ab5ac85cb8
Estiven HM <126631284+EstivenHM@users.noreply.github.com>
2025-02-22 11:25:59 -0600

    Se elimina adduser menu

--- Proyecto_final/RobertWEB/Menu.php
+++ Proyecto_final/RobertWEB/Menu.php
@@ -42,7 +42,7 @@ $username = $_SESSION['username'];
                     <label for="submenu-toggle" class="submenu-label"><i class='bx bxs-cog'></i>Mantenimientos</label>
                     <ul class="submenu-content">
                         <li><a href="Usuarios.php" id="usuarios"><i class='bx bxs-user-detail'></i>Usuarios</a></li>
-                        <li><a href="Crearusuario.php"id="Nuevo_usuario" ><i class='bx bxs-user-plus'></i>Nuevo usuario</a></li>
+                        <!--<li><a href="Crearusuario.php"id="Nuevo_usuario" ><i class='bx bxs-user-plus'></i>Nuevo usuario</a></li>-->
                         <li><a href="Roles.php" id="Roles"><i class='bx bxs-category'></i>Roles</a></li>
                         <li><li><a href="#" id="Permisos"><i class='bx bxs-key'></i>Permisos</a></li></li>
 
